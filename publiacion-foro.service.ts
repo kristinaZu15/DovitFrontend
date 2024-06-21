4e2f4da5f6cedd16a48387698337347ae3fb5722
HitaloCordova <u20211d902@upc.edu.pe>
2024-06-21 16:25:25 -0500

    Create publiacion-foro.service.ts
    publicaciones foro.servise

---
+++ publiacion-foro.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Publicaciones_Foro } from '../model/Publicaciones_Foro';
+import { HttpClient } from '@angular/common/http';
+
+const base_url = 'http://localhost:8084';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PubliacionForoService {
+  constructor(private http: HttpClient) {}
+
+  getPublicaciones_Foros() {
+    const endpoint = `${base_url}`;
+    return this.http.get<Publicaciones_Foro[]>(endpoint);
+  }
+  getPublicaciones_ForosById(id: any) {
+    const endpoint = `${base_url}/${id}`;
+    return this.http.get<Publicaciones_Foro[]>(endpoint);
+  }
+  savePublicaciones_Foro(body: any) {
+    const endpoint = `${base_url}`;
+    return this.http.post<Publicaciones_Foro>(endpoint, body);
+  }
+  updatePublicaciones_Foro(body: any, id: any) {
+    const endpoint = `${base_url}/${id}`;
+    return this.http.put<Publicaciones_Foro>(endpoint, body);
+  }
+  deletePublicaciones_Foro(id: any) {
+    const endpoint = `${base_url}/${id}`;
+    return this.http.delete<Publicaciones_Foro>(endpoint);
+  }
+}
